590c8a67a6e574c02e5a43e64cad964749566b6c
hasika-2037 <hasikasri67@gmail.com>
2024-08-29 14:21:16 +0530

    Add files via upload

---
+++ TestPanel.Designer.cs
@@ -0,0 +1,278 @@
+﻿namespace Testing
+{
+    partial class frmTestPanel
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnMessage = new System.Windows.Forms.Button();
+            this.btnVariable = new System.Windows.Forms.Button();
+            this.btnConstant = new System.Windows.Forms.Button();
+            this.btnCasting = new System.Windows.Forms.Button();
+            this.btnParameter = new System.Windows.Forms.Button();
+            this.btnSwitch = new System.Windows.Forms.Button();
+            this.btnArray = new System.Windows.Forms.Button();
+            this.btnEnum = new System.Windows.Forms.Button();
+            this.btnMath = new System.Windows.Forms.Button();
+            this.btnClass = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.rtxtOutput = new System.Windows.Forms.RichTextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.btnCalc = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnMessage
+            // 
+            this.btnMessage.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnMessage.Location = new System.Drawing.Point(21, 26);
+            this.btnMessage.Name = "btnMessage";
+            this.btnMessage.Size = new System.Drawing.Size(125, 66);
+            this.btnMessage.TabIndex = 0;
+            this.btnMessage.Text = "Message Usage";
+            this.btnMessage.UseVisualStyleBackColor = true;
+            this.btnMessage.Click += new System.EventHandler(this.btnMessage_Click);
+            // 
+            // btnVariable
+            // 
+            this.btnVariable.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnVariable.Location = new System.Drawing.Point(498, 26);
+            this.btnVariable.Name = "btnVariable";
+            this.btnVariable.Size = new System.Drawing.Size(125, 66);
+            this.btnVariable.TabIndex = 1;
+            this.btnVariable.Text = "Variable Usage";
+            this.btnVariable.UseVisualStyleBackColor = true;
+            this.btnVariable.Click += new System.EventHandler(this.btnVariable_Click);
+            // 
+            // btnConstant
+            // 
+            this.btnConstant.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnConstant.Location = new System.Drawing.Point(180, 107);
+            this.btnConstant.Name = "btnConstant";
+            this.btnConstant.Size = new System.Drawing.Size(125, 66);
+            this.btnConstant.TabIndex = 2;
+            this.btnConstant.Text = "Constant Usage";
+            this.btnConstant.UseVisualStyleBackColor = true;
+            this.btnConstant.Click += new System.EventHandler(this.btnConstant_Click);
+            // 
+            // btnCasting
+            // 
+            this.btnCasting.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCasting.Location = new System.Drawing.Point(180, 26);
+            this.btnCasting.Name = "btnCasting";
+            this.btnCasting.Size = new System.Drawing.Size(125, 66);
+            this.btnCasting.TabIndex = 3;
+            this.btnCasting.Text = "Type Casting Usage";
+            this.btnCasting.UseVisualStyleBackColor = true;
+            this.btnCasting.Click += new System.EventHandler(this.btnCasting_Click);
+            // 
+            // btnParameter
+            // 
+            this.btnParameter.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnParameter.Location = new System.Drawing.Point(657, 26);
+            this.btnParameter.Name = "btnParameter";
+            this.btnParameter.Size = new System.Drawing.Size(125, 66);
+            this.btnParameter.TabIndex = 4;
+            this.btnParameter.Text = "Parameter Usage";
+            this.btnParameter.UseVisualStyleBackColor = true;
+            this.btnParameter.Click += new System.EventHandler(this.btnParameter_Click);
+            // 
+            // btnSwitch
+            // 
+            this.btnSwitch.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSwitch.Location = new System.Drawing.Point(339, 107);
+            this.btnSwitch.Name = "btnSwitch";
+            this.btnSwitch.Size = new System.Drawing.Size(125, 66);
+            this.btnSwitch.TabIndex = 5;
+            this.btnSwitch.Text = "Switch Case Usage";
+            this.btnSwitch.UseVisualStyleBackColor = true;
+            this.btnSwitch.Click += new System.EventHandler(this.btnSwitch_Click);
+            // 
+            // btnArray
+            // 
+            this.btnArray.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnArray.Location = new System.Drawing.Point(339, 26);
+            this.btnArray.Name = "btnArray";
+            this.btnArray.Size = new System.Drawing.Size(125, 66);
+            this.btnArray.TabIndex = 6;
+            this.btnArray.Text = "Array with Loop Usage";
+            this.btnArray.UseVisualStyleBackColor = true;
+            this.btnArray.Click += new System.EventHandler(this.btnArray_Click);
+            // 
+            // btnEnum
+            // 
+            this.btnEnum.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnEnum.Location = new System.Drawing.Point(21, 107);
+            this.btnEnum.Name = "btnEnum";
+            this.btnEnum.Size = new System.Drawing.Size(125, 66);
+            this.btnEnum.TabIndex = 7;
+            this.btnEnum.Text = "Enum Usage";
+            this.btnEnum.UseVisualStyleBackColor = true;
+            this.btnEnum.Click += new System.EventHandler(this.btnEnum_Click);
+            // 
+            // btnMath
+            // 
+            this.btnMath.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnMath.Location = new System.Drawing.Point(498, 107);
+            this.btnMath.Name = "btnMath";
+            this.btnMath.Size = new System.Drawing.Size(125, 66);
+            this.btnMath.TabIndex = 8;
+            this.btnMath.Text = "Math Usage";
+            this.btnMath.UseVisualStyleBackColor = true;
+            this.btnMath.Click += new System.EventHandler(this.btnMath_Click);
+            // 
+            // btnClass
+            // 
+            this.btnClass.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnClass.Location = new System.Drawing.Point(657, 107);
+            this.btnClass.Name = "btnClass";
+            this.btnClass.Size = new System.Drawing.Size(125, 66);
+            this.btnClass.TabIndex = 9;
+            this.btnClass.Text = "Class Usage";
+            this.btnClass.UseVisualStyleBackColor = true;
+            this.btnClass.Click += new System.EventHandler(this.btnClass_Click);
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.Color.Yellow;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(809, 30);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "SDLC C# Coding Test Panel";
+            // 
+            // rtxtOutput
+            // 
+            this.rtxtOutput.ForeColor = System.Drawing.Color.Navy;
+            this.rtxtOutput.Location = new System.Drawing.Point(6, 27);
+            this.rtxtOutput.Name = "rtxtOutput";
+            this.rtxtOutput.ReadOnly = true;
+            this.rtxtOutput.Size = new System.Drawing.Size(508, 186);
+            this.rtxtOutput.TabIndex = 11;
+            this.rtxtOutput.Text = "";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnMessage);
+            this.groupBox1.Controls.Add(this.btnVariable);
+            this.groupBox1.Controls.Add(this.btnConstant);
+            this.groupBox1.Controls.Add(this.btnClass);
+            this.groupBox1.Controls.Add(this.btnCasting);
+            this.groupBox1.Controls.Add(this.btnMath);
+            this.groupBox1.Controls.Add(this.btnParameter);
+            this.groupBox1.Controls.Add(this.btnEnum);
+            this.groupBox1.Controls.Add(this.btnSwitch);
+            this.groupBox1.Controls.Add(this.btnArray);
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Location = new System.Drawing.Point(17, 56);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(804, 194);
+            this.groupBox1.TabIndex = 12;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "C# Basics";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.rtxtOutput);
+            this.groupBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox2.Location = new System.Drawing.Point(17, 267);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(522, 219);
+            this.groupBox2.TabIndex = 13;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Output View";
+            // 
+            // btnClose
+            // 
+            this.btnClose.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnClose.Location = new System.Drawing.Point(746, 486);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(75, 32);
+            this.btnClose.TabIndex = 14;
+            this.btnClose.Text = "Close";
+            this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // btnCalc
+            // 
+            this.btnCalc.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCalc.Location = new System.Drawing.Point(591, 294);
+            this.btnCalc.Name = "btnCalc";
+            this.btnCalc.Size = new System.Drawing.Size(208, 66);
+            this.btnCalc.TabIndex = 15;
+            this.btnCalc.Text = "Simple Calculator";
+            this.btnCalc.UseVisualStyleBackColor = true;
+            this.btnCalc.Click += new System.EventHandler(this.btnCalc_Click);
+            // 
+            // frmTestPanel
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(833, 525);
+            this.Controls.Add(this.btnCalc);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "frmTestPanel";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Test Panel";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnMessage;
+        private System.Windows.Forms.Button btnVariable;
+        private System.Windows.Forms.Button btnConstant;
+        private System.Windows.Forms.Button btnCasting;
+        private System.Windows.Forms.Button btnParameter;
+        private System.Windows.Forms.Button btnSwitch;
+        private System.Windows.Forms.Button btnArray;
+        private System.Windows.Forms.Button btnEnum;
+        private System.Windows.Forms.Button btnMath;
+        private System.Windows.Forms.Button btnClass;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RichTextBox rtxtOutput;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button btnClose;
+        private System.Windows.Forms.Button btnCalc;
+    }
+}
+
